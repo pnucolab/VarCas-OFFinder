374ea740fc537dcdc5a680f611108726084540b4
abyot2021 <89458721+abyot2021@users.noreply.github.com>
2023-04-21 10:56:56 +0900

    Add files via upload

---
+++ src/lib/Top.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Heading, P, A } from 'flowbite-svelte';
+</script>
+
+<Heading tag="h2" class="mt-5 mb-2">Cas-OFFinder</Heading>
+<Heading tag="h5" class="font-light mb-5">A webtool for searching potential off-target sites </Heading>
+<P class="mb-5">
+    Cas-OFFinder is a web-based tool for searching potential of-ftarget sites for different Cas genes in different organism's genome.
+</P>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports ={
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
+};
